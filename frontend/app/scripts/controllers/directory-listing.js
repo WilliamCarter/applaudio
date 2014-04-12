5aa2039b878916a4fffdc3161d10edcefc12b6a3
William Carter <william.carter@bbc.co.uk>
2014-04-12 19:17:29 +0100

    Added filter and title.

---
+++ frontend/app/scripts/controllers/directory-listing.js
@@ -0,0 +1,38 @@
+'use strict';
+
+define(["angular"], function (angular) {
+
+    console.log("Defining DirectoryListing module");
+
+    var DirectoryListing = angular.module("DirectoryListing", []);
+
+    console.log("Defining DirectoryListing controller");
+    DirectoryListing.controller('DirectoryListingCtrl', function ($rootScope, $scope, $routeParams, $http, $location) {
+
+        console.log("received url: " + $routeParams.url);
+        console.log($routeParams);
+
+        $rootScope.heading = "/" + $routeParams.url + "/";
+
+        $http.get("/api/listing/" + $routeParams.url).
+            success(function(data) {
+                $scope.listing = data.listing;
+            }).
+            error(function(){
+                console.log("Cannot find directory in /music/...");
+                $scope.listing = ["404"];
+                // TODO: 404 (+--0)~~~[~- - ]
+            });
+
+        $scope.navigate = function(directoryName) {
+            console.log("directoryName: " + directoryName);
+            $location.path($location.path() + "/" + directoryName);
+        };
+
+    });
+
+    console.log("Defined DirectoryListing module");
+
+    return DirectoryListing;
+
+});
