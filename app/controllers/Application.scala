bd76f74820cea4c7a52e479c3b32093919e21373
William Carter <williamcarter1988@gmail.com>
2014-05-11 14:06:11 +0100

    Different static resource locations for DEV and PROD modes.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,6 @@ package controllers
 
 import play.api.mvc._
 
-/**
- * Created by cartew01 on 21/03/2014.
- */
 object Application extends Controller{
 
   def test(url: String) = Action {
--- build.sbt
+++ build.sbt
@@ -1,11 +1,1 @@
-name := "applaudio"
-
-version := "1.0-SNAPSHOT"
-
-libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
-  cache
-)     
-
 play.Project.playScalaSettings
