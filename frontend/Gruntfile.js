62a115b4c0d91bc904895a92e4d2ae0a839ca36a
William Carter <williamcarter1988@gmail.com>
2014-11-24 19:31:56 +0000

    Added 'expr' to jshint. Fixed require optimisation configuration

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -140,10 +140,19 @@ module.exports = function (grunt) {
                     name: "main",
                     out: "<%= paths.dist %>/main.js",
                     paths: {
-                        angular: "empty:",
-                        angularAnimate: "empty:",
-                        angularRoute: "empty:",
-                        angularMocks: "empty:"
+                        angular: "bower_components/angular/angular",
+                        angularAnimate: "bower_components/angular-animate/angular-animate",
+                        angularRoute: "bower_components/angular-route/angular-route",
+                        angularUiTree: "bower_components/angular-ui-tree/dist/angular-ui-tree",
+                        catext: "bower_components/catext/src",
+                        es5Shim: "bower_components/es5-shim/es5-shim",
+                        howler: "bower_components/howler/howler"
+                    },
+                    shim: {
+                        angular: { exports: "angular" },
+                        angularAnimate: ["angular"],
+                        angularRoute: ["angular"],
+                        angularUiTree: ["angular", "es5Shim"]
                     }
 
                 }
