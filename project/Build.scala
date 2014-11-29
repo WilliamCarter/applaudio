ac2a468814f3e22011ac36f647064378a3514376
William Carter <williamcarter1988@gmail.com>
2014-11-29 14:59:38 +0000

    Updated TODO

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object ApplicationBuild extends Build {
   println("Loading build")
 
   val appName = "applaudio"
-  val appVersion = "1.0.0"
+  val appVersion = "1.0.1"
   val appDependencies = Seq(
     "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
     "org.mockito" % "mockito-all" % "1.9.5" % "test"
